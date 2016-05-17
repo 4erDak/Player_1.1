6f868ebf65dcc8beab96bdc693e07fd041b5ffb8
mrlead <krendelek1509999@gmail.com>
2016-05-17 17:20:49 +0300

    Я загрузил часть проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace audio_player_PI_15_B
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new A_Player.Form1());
+        }
+    }
+}
